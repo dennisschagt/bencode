a7790f987bbe29436efde6a99c095ed7b8ba27d7
dennisschagt <dennisschagt@gmail.com>
2014-12-04 01:17:21 +0100

    Added doesKeyExist() to Dictionary

--- dictionary.cpp
+++ dictionary.cpp
@@ -33,6 +33,15 @@ namespace Bencode {
     void Dictionary::addKeyValuePair(KeyValuePair pair) {
         this->keyValues.push_back(pair);
     }
+    
+    bool Dictionary::doesKeyExist(const Benstring& key) {
+        for (KeyValuePair& keyValue : this->keyValues) {
+            if (*keyValue.getKey() == key) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     std::shared_ptr<Element> Dictionary::getValue(const Benstring& key) {
         for (KeyValuePair& keyValue : this->keyValues) {
@@ -45,5 +54,9 @@ namespace Bencode {
     
     std::shared_ptr<Benstring> Dictionary::getValueBenstring(const Benstring& key) {
         return std::dynamic_pointer_cast<Benstring>(this->getValue(key));
+    }
+    
+    std::shared_ptr<List> Dictionary::getValueList(const Benstring& key) {
+        return std::dynamic_pointer_cast<List>(this->getValue(key));
     }
 }
